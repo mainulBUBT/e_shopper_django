b50ded78d98514b9fc818b8b8baf9bbcf3b7bcfb
mainulBUBT <shopniilmihan@gmail.com>
2022-03-28 15:39:20 +0600

    cart functionalities

--- Ecommerce_App/Ecommerce_App/views.py
+++ Ecommerce_App/Ecommerce_App/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.shortcuts import redirect, render
 from apps.models import Category, Product, UserForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
 
 
 def index(request):
@@ -37,3 +39,47 @@ def register(request):
     
     dictn = {'form': form }
     return render(request, 'registration/register.html', context=dictn)
+
+
+@login_required(login_url="/users/login")
+def cart_add(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("index")
+
+
+@login_required(login_url="/users/login")
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/users/login")
+def item_increment(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/users/login")
+def item_decrement(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.decrement(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/users/login")
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/users/login")
+def cart_detail(request):
+    return render(request, 'cart/cart_detail.html')
